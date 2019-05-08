12cd0004e87dc98bb80c878e8bdea68c2bb19611
Valery Zubovich <aspelled1701@gmail.com>
2019-05-08 20:56:04 +0300

    Update Task2_2.java

--- Task2/Task2_2.java
+++ Task2/Task2_2.java
@@ -5,16 +5,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter dragon age: ");
         int age = sc.nextInt();
-        int head;
-        if (age > 300) {
-            head = 600 + 200 + (age - 300);
-        } else
-            if (age >= 200 && age < 300) {
-            head = 600 + (age - 200) * 2;
+        if (age <= 0) {
+            System.out.println("Wrong age(age should be >0)");
         } else {
-            head = age * 3;
+            int head;
+            if (age > 300) {
+                head = 600 + 200 + (age - 300);
+            } else if (age >= 200 && age < 300) {
+                head = 600 + (age - 200) * 2;
+            } else {
+                head = age * 3;
+            }
+            int eyes = head * 2;
+            System.out.println("Dragon is " + age + " years old, has " + head + " heads and " + eyes + " eyes");
         }
-        int eyes = head * 2;
-        System.out.println("Dragon is " + age + " years old, has " + head +" heads and " + eyes + " eyes");
     }
 }
